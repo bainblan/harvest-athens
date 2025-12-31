ea2a712807873c70989b39f9ad035544ed116562
bainblan <Baines.Blanton@uga.edu>
2025-12-31 09:57:58 -0500

    adjusted title and landing page font for mobile

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -5,8 +5,8 @@ export default function Landing() {
     return (
         <div className="relative flex flex-col justify-center items-center bg-cover bg-fixed bg-center w-full h-[1000px]" style={{ backgroundImage: `url(${downtown.src})` }}>
             <div className="absolute inset-0 bg-black/40"></div>
-            <FadeIn className="relative italic text-white text-center font-playfair font-bold text-[96px] md:w-[800px]">
-                The Harvest is Plentiful but the Workers are Few.<br /><span className="text-[48px]">Luke 10:2</span>
+            <FadeIn className="relative italic text-white text-center font-playfair font-bold text-[48px] md:text-[96px] md:w-[800px]">
+                The Harvest is Plentiful but the Workers are Few.<br /><span className="text-[32px] md:text-[48px]">Luke 10:2</span>
             </FadeIn>
         </div>
     );
--- components/Title.tsx
+++ components/Title.tsx
@@ -3,16 +3,16 @@ import FadeIn from "../components/FadeIn"
 export default function Title() {
     return (
         <section className="flex w-full h-fit bg-[#FFCBA7] justify-center px-[10px] pb-[50px]">
-            <div className="flex flex-col w-[854px] h-[754px] justify-between items-center">
-                <FadeIn direction="down" className="text-[#5F120C] text-center font-bold font-playfair text-[96px] drop-shadow-[5px_3px_4px_rgba(0,0,0,0.7)]">
+            <div className="flex flex-col w-[854px] h-fit gap-[50px] justify-between items-center">
+                <FadeIn direction="down" className="text-[#5F120C] text-center font-bold font-playfair text-[48px] md:text-[96px] drop-shadow-[5px_3px_4px_rgba(0,0,0,0.7)]">
                     HARVEST<br />
-                    <h1 className="text-[64px]"> -ATHENS-</h1>
+                    <h1 className=" text-[36px] md:text-[64px]"> -ATHENS-</h1>
                 </FadeIn>
-                <FadeIn className="text-[#5F120C] animate-bounce font-playfair text-center text-[48px] font-bold">
+                <FadeIn className="text-[#5F120C] animate-bounce font-playfair text-center text-[24px] md:text-[48px] font-bold">
                     Fridays @ 8:30<br />
                     THE ROOK & PAWN
                 </FadeIn>
-                <FadeIn direction="down" className="text-[#6A6A6A] text-center md:text-left px-[30px] font-lato text-[32px]">
+                <FadeIn direction="down" className="text-[#6A6A6A] text-center md:text-left px-[30px] font-lato text-[24px] md:text-[32px]">
                     We seek to be a <b>Christ-centered</b> community marked by prayer that lovingly brings the Gospel of Jesus Christ to the lost, poor, broken, and oppressed of downtown Athens, while abiding in the love of Christ.
                 </FadeIn>
             </div>
