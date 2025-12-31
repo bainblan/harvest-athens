26b3357e37373cb842aef07c6ef218199cecdac7
bainblan <Baines.Blanton@uga.edu>
2025-12-31 10:17:22 -0500

    fixing responsiveness on first page

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,11 +6,11 @@ export default function Contact() {
     return (
         <section className="flex bg-[#FFCBA7] justify-center px-[10px] py-[100px] gap-[10px]">
             <div className="flex flex-col items-center md:flex-row w-fit h-fit justify-between gap-[70px]">
-                <FadeIn direction="left" className="flex flex-col gap-[16px] w-[485px] h-full justify-between">
-                    <p className="font-bold font-playfair text-[30px] text-[#BF603F] text-right">
+                <FadeIn direction="left" className="flex flex-col gap-[16px] w-[375px] md:w-[485px] h-full justify-between">
+                    <p className="font-bold font-playfair text-[22px] md:text-[30px] text-[#BF603F] text-right">
                         <span className="text-[#5F120C]">More details</span> regarding logistics, safety and resources will be shared in the <span className="text-[#5F120C]">GroupMe</span>, join if you&apos;re interested!
                     </p>
-                    <p className="font-semibold font-Lato text-[18px] text-[#5E5E5E] text-right">
+                    <p className="font-semibold font-Lato text-[14px] md:text-[18px] text-[#5E5E5E] text-right">
                         If you don&apos;t feel comfortable or ready to share the Gospel in that manner, we&apos;d still love to have you! Being only a member of the prayer team is as valuable, if not more valuable! We&apos;d also love to equip you and help get you to the point that Gospel conversations feel both comfortable and natural!
                     </p>
                 </FadeIn>
--- components/How.tsx
+++ components/How.tsx
@@ -2,25 +2,25 @@ import FadeIn from "../components/FadeIn";
 
 export default function How() {
     return (
-        <section className="flex bg-[#FFE2CE] justify-center px-[10px] py-[100px] gap-[10px]">
+        <section className="flex bg-[#FFE2CE] justify-center px-[10px] py-[80px] gap-[10px]">
             <div className="flex flex-col items-center md:flex-row gap-[50px] w-fit h-fit justify-between">
-                <FadeIn direction="up" className="flex flex-col gap-[16px] w-[465px] md:w-[414px] h-full justify-between">
-                    <p className="font-bold font-playfair text-[48px] text-black text-center">
+                <FadeIn direction="up" className="flex flex-col gap-[16px] w-[375px] md:w-[465px] md:w-[414px] h-full justify-between">
+                    <p className="font-bold font-playfair text-[32px] md:text-[48px] text-black text-center">
                         How?
                     </p>
-                    <p className="font-regular font-playfair text-[40px] text-black text-center">
+                    <p className="font-regular font-playfair text-[24px] md:text-[40px] text-black text-center">
                         Prayer!
                     </p>
-                    <p className="font-medium font-Lato text-[22px] text-black text-right">
+                    <p className="font-medium font-Lato text-[18px] md:text-[22px] text-black text-right">
                         Psalm 127:1<br />
                         Unless the Lord builds the house, those who build it labor in vain. Unless the Lord watches over the city, the watchman stays awake in vain.
                     </p>
                 </FadeIn>
-                <FadeIn direction="right" className="flex flex-col w-[465px] h-full gap-[16px] justify-between">
-                    <p className="font-bold font-playfair text-[30px] text-[#BF603F] text-left">
+                <FadeIn direction="right" className="flex flex-col w-[375px] md:w-[465px] h-full gap-[16px] justify-between">
+                    <p className="font-bold font-playfair text-[22px] md:text-[30px] text-[#BF603F] text-left">
                         <span className="text-[#5F120C]">Each night</span> we will begin in a collective time of prayer, followed by a scripture reading, then a group break out time of prayer. 
                     </p>
-                    <p className="font-semibold font-Lato text-[18px] text-[#5E5E5E] text-left">
+                    <p className="font-semibold font-Lato text-[14px] md:text-[18px] text-[#5E5E5E] text-left">
                         No matter how many people we have, we will always have an equal number of people actively praying at the coffee shop and evangelizing on the streets. Groups that are out sharing the Gospel should also remain in Christ when starting and having conversations. Without this our works are fruitless.
                     </p>
                 </FadeIn>
